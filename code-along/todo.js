71b0f7f4d7aacb3a16fefd7dd64157d696f109f1
RickCrowley <rick.crowley@kellogg.northwestern.edu>
2021-02-18 21:04:16 -0600

    my attempt

--- code-along/todo.js
+++ code-along/todo.js
@@ -1,76 +1,105 @@
 // Step 2: Change main event listener from DOMContentLoaded to 
 // firebase.auth().onAuthStateChanged and move code that 
 // shows login UI to only show when signed out
-document.addEventListener('DOMContentLoaded', async function(event) {
-  
+// document.addEventListener('DOMContentLoaded', async function(event) {
+firebase.auth().onAuthStateChanged(async function (user) {
   let db = firebase.firestore()
 
-  document.querySelector('form').addEventListener('submit', async function(event) {
-    event.preventDefault()
+  if (user) {
+    console.log('user logged in')
 
-    let todoText = document.querySelector('#todo').value
+    db.collection('users').doc(user.uid).set({
+      name: user.displayName,
+      email: user.email
+    })
 
-    if (todoText.length > 0) {
-      let docRef = await db.collection('todos').add({
-        text: todoText
-      })
+    document.querySelector('.sign-in-or-sign-out').innerHTML = `
+      <a href="#" class="sign-out text-pink-500 underline">Sign out</a>
+    `
+    document.querySelector('.sign-out').addEventListener('click', function(event) {
+      event.preventDefault()
+      firebase.auth().signOut()
+      document.location.href = `todo.html`
+    })
 
-      let todoId = docRef.id
-      console.log(`new todo with ID ${todoId} created`)
+    document.querySelector('form').addEventListener('submit', async function (event) {
+      event.preventDefault()
+  
+      let todoText = document.querySelector('#todo').value
+  
+      if (todoText.length > 0) {
+        let docRef = await db.collection('todos').add({
+          text: todoText,
+          userId: user.uid
+        })
+  
+        let todoId = docRef.id
+        console.log(`new todo with ID ${todoId} created`)
+  
+        document.querySelector('.todos').insertAdjacentHTML('beforeend', `
+          <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
+            <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
+            ${todoText}
+          </div>
+        `)
+  
+        document.querySelector(`.todo-${todoId} .done`).addEventListener('click', async function (event) {
+          event.preventDefault()
+          document.querySelector(`.todo-${todoId}`).classList.add('opacity-20')
+          await db.collection('todos').doc(todoId).delete()
+        })
+        document.querySelector('#todo').value = ''
+      }
+    })
+  
+    // let querySnapshot = await db.collection('todos').get()
+    // console.log(`Number to todos in collection: ${querySnapshot.size}`)
 
+    let querySnapshot = await db.collection('todos').where(`userId`, `==`, user.uid).get()
+  
+    let todos = querySnapshot.docs
+    for (let i = 0; i < todos.length; i++) {
+      let todoId = todos[i].id
+      let todo = todos[i].data()
+      let todoText = todo.text
+  
       document.querySelector('.todos').insertAdjacentHTML('beforeend', `
         <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
           <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
           ${todoText}
         </div>
       `)
-
-      document.querySelector(`.todo-${todoId} .done`).addEventListener('click', async function(event) {
+  
+      document.querySelector(`.todo-${todoId} .done`).addEventListener('click', async function (event) {
         event.preventDefault()
         document.querySelector(`.todo-${todoId}`).classList.add('opacity-20')
         await db.collection('todos').doc(todoId).delete()
       })
-      document.querySelector('#todo').value = ''
     }
-  })
 
-  let querySnapshot = await db.collection('todos').get()
-  console.log(`Number to todos in collection: ${querySnapshot.size}`)
+  } else {
 
-  let todos = querySnapshot.docs
-  for (let i=0; i<todos.length; i++) {
-    let todoId = todos[i].id
-    let todo = todos[i].data()
-    let todoText = todo.text
+    document.querySelector(`form`).classList.add(`hidden`)
 
-    document.querySelector('.todos').insertAdjacentHTML('beforeend', `
-      <div class="todo-${todoId} py-4 text-xl border-b-2 border-purple-500 w-full">
-        <a href="#" class="done p-2 text-sm bg-green-500 text-white">✓</a>
-        ${todoText}
-      </div>
-    `)
+    // Step 1: Un-comment to add FirebaseUI Auth
+    // // Initializes FirebaseUI Auth
+    let ui = new firebaseui.auth.AuthUI(firebase.auth())
 
-    document.querySelector(`.todo-${todoId} .done`).addEventListener('click', async function(event) {
-      event.preventDefault()
-      document.querySelector(`.todo-${todoId}`).classList.add('opacity-20')
-      await db.collection('todos').doc(todoId).delete()
-    })
+    // FirebaseUI configuration
+    let authUIConfig = {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      signInSuccessUrl: 'todo.html'
+    }
+
+    // Starts FirebaseUI Auth
+    ui.start('.sign-in-or-sign-out', authUIConfig)
+
+    console.log('no user')
   }
 
-  // Step 1: Un-comment to add FirebaseUI Auth
-  // // Initializes FirebaseUI Auth
-  // let ui = new firebaseui.auth.AuthUI(firebase.auth())
 
-  // // FirebaseUI configuration
-  // let authUIConfig = {
-  //   signInOptions: [
-  //     firebase.auth.EmailAuthProvider.PROVIDER_ID
-  //   ],
-  //   signInSuccessUrl: 'todo.html'
-  // }
-
-  // // Starts FirebaseUI Auth
-  // ui.start('.sign-in-or-sign-out', authUIConfig)
 })
 
 // Step 3: Hide the form when signed-out
--- lab/kelloggram.js
+++ lab/kelloggram.js
@@ -3,10 +3,11 @@ let db = firebase.firestore()
 // Change main event listener from DOMContentLoaded to 
 // firebase.auth().onAuthStateChanged and move code that 
 // shows login UI to only show when signed out
-firebase.auth().onAuthStateChanged(async function(user) {
+firebase.auth().onAuthStateChanged(async function (user) {
   if (user) {
     // Signed in
     console.log('signed in')
+    // document.querySelector('#username').classList.add('hidden')
 
     // Ensure the signed-in user is in the users collection
     db.collection('users').doc(user.uid).set({
@@ -18,22 +19,22 @@ firebase.auth().onAuthStateChanged(async function(user) {
     document.querySelector('.sign-in-or-sign-out').innerHTML = `
       <button class="text-pink-500 underline sign-out">Sign Out</button>
     `
-    document.querySelector('.sign-out').addEventListener('click', function(event) {
+    document.querySelector('.sign-out').addEventListener('click', function (event) {
       console.log('sign out clicked')
       firebase.auth().signOut()
       document.location.href = 'kelloggram.html'
     })
 
     // Listen for the form submit and create/render the new post
-    document.querySelector('form').addEventListener('submit', async function(event) {
+    document.querySelector('form').addEventListener('submit', async function (event) {
       event.preventDefault()
-      let postUsername = document.querySelector('#username').value
+      let postUsername = user.displayName
       let postImageUrl = document.querySelector('#image-url').value
       let postNumberOfLikes = 0
-      let docRef = await db.collection('posts').add({ 
-        username: postUsername, 
-        imageUrl: postImageUrl, 
-        likes: 0,
+      let docRef = await db.collection('posts').add({
+        username: postUsername,
+        imageUrl: postImageUrl,
+        // likes: 0,
         created: firebase.firestore.FieldValue.serverTimestamp()
       })
       let postId = docRef.id // the newly created document's ID
@@ -43,12 +44,13 @@ firebase.auth().onAuthStateChanged(async function(user) {
     // Render all posts when the page is loaded
     let querySnapshot = await db.collection('posts').orderBy('created').get()
     let posts = querySnapshot.docs
-    for (let i=0; i<posts.length; i++) {
+    for (let i = 0; i < posts.length; i++) {
       let postId = posts[i].id
       let postData = posts[i].data()
       let postUsername = postData.username
       let postImageUrl = postData.imageUrl
-      let postNumberOfLikes = postData.likes
+      let querySnapshot = await db.collection(`likes`).where(`postId`, `==`, postId).get()
+      let postNumberOfLikes = querySnapshot.size
       renderPost(postId, postUsername, postImageUrl, postNumberOfLikes)
     }
 
@@ -92,15 +94,29 @@ async function renderPost(postId, postUsername, postImageUrl, postNumberOfLikes)
       </div>
     </div>
   `)
-  document.querySelector(`.post-${postId} .like-button`).addEventListener('click', async function(event) {
+  document.querySelector(`.post-${postId} .like-button`).addEventListener('click', async function (event) {
     event.preventDefault()
     console.log(`post ${postId} like button clicked!`)
-    let existingNumberOfLikes = document.querySelector(`.post-${postId} .likes`).innerHTML
-    let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
-    document.querySelector(`.post-${postId} .likes`).innerHTML = newNumberOfLikes
-    await db.collection('posts').doc(postId).update({
-      likes: firebase.firestore.FieldValue.increment(1)
-    })
+
+
+    let currentUser = firebase.auth().currentUser
+
+    let querySnapshot = await db.collection(`likes`).where(`postId`, `==`, postId).where(`userId`, `==`, currentUser.uid).get()
+
+    if (querySnapshot.size == 0) {
+      await db.collection(`likes`).add({
+        postId: postId,
+        userId: currentUser.uid
+      })
+      let existingNumberOfLikes = document.querySelector(`.post-${postId} .likes`).innerHTML
+      let newNumberOfLikes = parseInt(existingNumberOfLikes) + 1
+      document.querySelector(`.post-${postId} .likes`).innerHTML = newNumberOfLikes
+    } 
+
+
+    // await db.collection('posts').doc(postId).update({
+    //   likes: firebase.firestore.FieldValue.increment(1)
+    // })
   })
 }
 
